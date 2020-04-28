8067942727df0ec31aff058df9895afe8eeceed1
Diaa <ditek@users.noreply.github.com>
2020-04-28 02:16:13 +0200

    Support creating todos and storing them in the database

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,30 @@
 import 'source-map-support/register'
-
+import * as uuid from 'uuid'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const item = {
+    id: uuid.v4(),
+    ...newTodo
+  }
+  await docClient.put({
+    TableName: tableName,
+    Item: item
+  }).promise();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(item)
+  }
 }
