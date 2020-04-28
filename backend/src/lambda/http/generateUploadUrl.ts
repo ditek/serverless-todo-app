bd1d3bb31c34323b34e2e4f6a18d50e471854e10
Diaa <ditek@users.noreply.github.com>
2020-04-28 12:35:26 +0200

    Add upload image functionality

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,62 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.TODOS_TABLE;
+const bucketName = process.env.IMAGES_S3_BUCKET;
+const region = process.env.REGION;
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4',
+  region: 'us-east-1',
+  params: { Bucket: bucketName }
+});
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  try {
+    // Generate image name
+    const imageName = uuid.v4()
+    // The URL looks like "https://s3-bucket-name.s3.eu-west-2.amazonaws.com/image.png"
+    const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${imageName}`
+
+    // Obtain signed URL
+    const urlParams = {
+      Bucket: bucketName,
+      Key: imageName,
+      Expires: (60 * 5)
+    };
+
+    const uploadUrl = s3.getSignedUrl('putObject', urlParams);
+
+    await docClient.update({
+      TableName: tableName,
+      Key: { id: todoId },
+      UpdateExpression: "set attachmentUrl = :url",
+      ExpressionAttributeValues: {
+        ":url": imageUrl
+      },
+      ReturnValues: "UPDATED_NEW"
+    }).promise()
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({ uploadUrl })
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(error)
+    }
+  }
 }
